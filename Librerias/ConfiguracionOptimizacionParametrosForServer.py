16a96f79f0873fe7f8c53bd09f99b9ca8ca50f5f
carlos <c.gallardog@alumnos.upm.es>
2022-06-22 12:02:59 +0200

    initial parameters

--- Librerias/ConfiguracionOptimizacionParametrosForServer.py
+++ Librerias/ConfiguracionOptimizacionParametrosForServer.py
@@ -7,7 +7,7 @@ class CombinacionParametrosRedOptimizar():
         self.neurons_layer_1 = [32, 128, 32]
         self.neurons_layer_2 = [0, 0, 0]
         self.neurons_layer_3 = [32, 128, 32]
-        self.fixed_neurons_1 = 8
+        self.fixed_neurons_1 = 16
         self.fixed_neurons_2 = 8
         self.fixed_neurons_3 = 8
         # self.combinaciones_optimizer_alg = [1, 2, 3]    # optimizer_alg == 1: 'Adam', optimizer_alg == 2: 'SGD', optimizer_alg == 3: 'RMSprop'
