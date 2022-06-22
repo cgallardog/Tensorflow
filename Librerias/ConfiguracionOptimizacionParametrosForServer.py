c403b007c5ff337c4a3c3957497695b52df45ea0
carlos <c.gallardog@alumnos.upm.es>
2022-06-22 11:53:53 +0200

    combintaion [1024, 512, 256] l3

--- Librerias/ConfiguracionOptimizacionParametrosForServer.py
+++ Librerias/ConfiguracionOptimizacionParametrosForServer.py
@@ -3,20 +3,20 @@ class CombinacionParametrosRedOptimizar():
     def __init__(self):
         self.combinaciones_t_seq = [12]  # Input dimension a la red
         self.combinaciones_q = [5]  # Prediction horizon is H+q
-        self.combinaciones_n_layers = 5
+        self.combinaciones_n_layers = 0
         self.neurons_layer_1 = [32, 128, 32]
         self.neurons_layer_2 = [0, 0, 0]
         self.neurons_layer_3 = [32, 128, 32]
-        self.fixed_neurons_1 = 32
-        self.fixed_neurons_2 = 32
-        self.fixed_neurons_3 = 256
+        self.fixed_neurons_1 = 8
+        self.fixed_neurons_2 = 8
+        self.fixed_neurons_3 = 8
         # self.combinaciones_optimizer_alg = [1, 2, 3]    # optimizer_alg == 1: 'Adam', optimizer_alg == 2: 'SGD', optimizer_alg == 3: 'RMSprop'
         self.combinaciones_optimizer_alg = [1]  # optimizer_alg == 1: 'Adam', optimizer_alg == 2: 'SGD', optimizer_alg == 3: 'RMSprop'
         self.combinaciones_name_optimizer = ['Adam', 'SGD', 'RMSprop']
         self.combinaciones_lr = [0.01, 0.001]  # learning rate
         self.fixed_lr = 0.001
-        self.dropout = 0.2
-        self.recurrent_dropout = 0.5
+        self.dropout = 0.0
+        self.recurrent_dropout = 0.0
 
     def get_combinaciones_tseq(self):
         return self.combinaciones_t_seq
