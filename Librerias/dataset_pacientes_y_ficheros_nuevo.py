8a484049df9911ac669be5ad71ecdbe8a3f64a8f
carlos <c.gallardog@alumnos.upm.es>
2022-09-27 13:45:53 +0200

    no deberia conn mas datos

--- Librerias/dataset_pacientes_y_ficheros_nuevo.py
+++ Librerias/dataset_pacientes_y_ficheros_nuevo.py
@@ -70,9 +70,9 @@ def get_dataset():
            "653", "655"]  # Paciente 596 training
     P24 = ["657", "659", "660", "661", "663", "664"]  # Paciente 596 testing
 
-    train_data = P13 + P15 + P17 + P19 + P21 + P23  # dataset of training (impares)
+    train_data = P13 + P15 + P17 + P19 + P21 + P23 + P1 + P3 + P5 + P7 + P9 + P11  # dataset of training (impares)
     #   train_data = P1  # dataset of training
-    eval_data = P2 + P4 + P6 + P8 + P10 + P12 + P1 + P3 + P5 + P7 + P9 + P11# dataset of evaluation (pares, excepto P2 que dejo para test)
+    eval_data = P2 + P4 + P6 + P8 + P10 + P12 # dataset of evaluation (pares, excepto P2 que dejo para test)
     #   eval_data = P2  # dataset of evaluation
     test_data = P14 + P16 + P18 + P20 + P22 + P24  # dataset of testing (to prevent overfitting)
     all_dataset = train_data + test_data
